9b74fef26bf4b8b357e3552396a97c028ed47952
HoangKyPham <hoangkypham1682@gmail.com>
2024-03-21 15:38:54 +0700

    update

--- script/script.js
+++ script/script.js
@@ -2,13 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const userLoginBtn = document.querySelector('.user-login');
     const dialog = document.querySelector('.dialog');
 
-    // Hàm xử lý khi click vào nút user login
     function handleUserLoginClick(event) {
         event.preventDefault();
-        dialog.style.display = 'flex'; // Hiển thị dialog
+        dialog.style.display = 'flex'; 
     }
 
-    // Thêm sự kiện click vào nút user login
     userLoginBtn.addEventListener('click', handleUserLoginClick);
 
 });
@@ -17,12 +15,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const dialog = document.querySelector('.dialog');
     const closeBtn = dialog.querySelector('.dialog-close__btn');
 
-    // Hàm xử lý khi click vào nút close
     function handleCloseButtonClick(event) {
         event.preventDefault();
-        dialog.style.display = 'none'; // Ẩn đi dialog
+        dialog.style.display = 'none'; 
     }
 
-    // Thêm sự kiện click vào nút close
     closeBtn.addEventListener('click', handleCloseButtonClick);
 });
